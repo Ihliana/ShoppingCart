c336889b03ce25832a917add976ea9b9f56b3ad2
Ihliana <Ihnatco@protonmail.com>
2023-09-01 16:29:02 +0200

    adding 'faker'
    library that generates fake (but reasonable) data

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,9 +1,21 @@
 import React, {createContext} from 'react'
-
+import {faker} from '@faker-js/faker';
 
 const Cart = createContext()
 
 export const Context = ({children}) => {
+
+    const products = [...Array(20)].map(() => ({
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        image: faker.image.url(),
+        fastDelivery: faker.datatype.boolean(),
+        inStock: faker.helpers.arrayElement([0,3,5,6,7]),
+        ratings: faker.helpers.arrayElement([1,2,3,4,5])
+      }));
+
+      console.log(products)
   return (
         <Cart.Provider>
                 {children}
